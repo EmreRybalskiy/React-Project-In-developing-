4b93ce582a81aff9fd39438eb15e9fefe0e6c5d9
Emre <bQnote197@gmail.com>
2021-05-04 11:34:21 +0300

    5

--- src/components/Main/Catalog/Catalog.jsx
+++ src/components/Main/Catalog/Catalog.jsx
@@ -1,6 +1,33 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
 import "./catalog.css";
 
 export const Catalog = () => {
-  return <div className="catalog-holder">Catalog</div>;
+  const query = gql`
+    query gfa {
+      GoodFind(query: "[{}]") {
+        images {
+          url
+        }
+      }
+    }
+  `;
+
+  const link = "//shop-roles.asmer.fs.a-level.com.ua/";
+
+  const { data } = useQuery(query);
+  console.log(data);
+  return (
+    <div className="catalog-holder">
+      <div>a</div>
+      <div>
+        {data &&
+          data.GoodFind.filter((product) => product.images).map((product) => {
+            return <img src={link + product.images[0].url} />;
+          })}
+      </div>
+    </div>
+  );
 };
