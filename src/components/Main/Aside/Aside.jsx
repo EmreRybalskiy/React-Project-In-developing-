e3b6389f35c906e34b068238f0b97fafe4160fa6
Emre <bQnote197@gmail.com>
2021-05-05 20:32:54 +0300

    45

--- src/components/Main/Aside/Aside.jsx
+++ src/components/Main/Aside/Aside.jsx
@@ -5,15 +5,15 @@ import { Input } from "../../Input/Input.jsx";
 import "./aside.css";
 
 export const Aside = () => {
-  const [state, setState] = useState(false);
+  // const [state, setState] = useState(false);
 
-  const handleClick = () => {
-    if (state) {
-      setState(false);
-    }
-    setState(true);
-  };
-  console.log(state);
+  // const handleClick = () => {
+  //   if (state) {
+  //     setState(false);
+  //   }
+  //   setState(true);
+  // };
+  // console.log
   return (
     <aside className="aside">
       <h5 className="price">Price</h5>
@@ -21,24 +21,17 @@ export const Aside = () => {
       <Input type="text" class="sum prev" />
       <span className="to">to</span>
       <Input type="text" class="sum after" />
-      <h5
-        className="sort"
-        onClick={() => {
-          handleClick();
-        }}
-      >
-        Sort
-      </h5>
-      <ul className="sort-menu price">
+      <div className="item">
+        <h5 className="field-name" className="sort">
+          Sort
+        </h5>
+        <div className="vision-menu">
+          <div className="arrow"></div>
+        </div>
+      </div>
+      <ul className="sort-menu">
         <li>Price high to low</li>
         <li>Price low to high</li>
-      </ul>
-      <h5 className="category">Category</h5>
-      <ul className="sort-menu">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
       </ul>
     </aside>
   );
